272aed8779a41bef5ad5255f40e9e14ce814b7bc
Lucasgomesss <168254568+Lucasgomesss@users.noreply.github.com>
2025-12-05 11:28:37 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs')
+app.set('views',"./views")
+
 // Define your routes
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Express on Vercel!' });
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
