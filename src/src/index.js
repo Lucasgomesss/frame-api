be6bec771bfdf346f71b57ab431c5ba67f311b82
Lucasgomesss <168254568+Lucasgomesss@users.noreply.github.com>
2025-12-09 08:52:51 -0400

    Add files via upload

---
+++ src/src/index.js
@@ -0,0 +1,21 @@
+// Use "type: commonjs" in package.json to use CommonJS modules
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs')
+app.set('views','./views')
+
+// Define your routes
+require("../route/home")(app)
+require("../db/dbconnect")(app)
+require("../route/delnoticias")(app)
+require("../route/getnoticias")(app)
+require("../route/postnoticias")(app)
+require("../route/putnoticias")(app)
+require("../route/getidnoticias")(app)
+require("../route/gettiponoticias")(app)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
