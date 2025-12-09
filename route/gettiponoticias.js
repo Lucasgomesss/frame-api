5df9ef6f622f2fc26ae36d98d0c355e2ea6d0300
Lucasgomesss <168254568+Lucasgomesss@users.noreply.github.com>
2025-12-09 09:02:55 -0400

    Add files via upload

---
+++ route/gettiponoticias.js
@@ -0,0 +1,15 @@
+const { ObjectId } = require("mongodb");
+module.exports = (app) => {
+    app.get('/noticias/tipo/:tiponoticia', async (req, res) => {
+        try {
+            const tiponoticia = req.params.tiponoticia
+            await app.DBClient.connect(); //realizar a conexão com o banco 
+            const noticias = await app.DBClient.db('portalnoticias')
+                .collection('noticias').find({tiponoticia: new RegExp(tiponoticia, 'i')}).toArray();
+            res.json(noticias)
+        } finally {
+            // Ensures that the client will close when you finish/error
+            await app.DBClient.close();
+        }
+    })
+}
