dc86b06cbcf4887a3b8497ba2cc1c6c0a5a1ec54
Lucasgomesss <168254568+Lucasgomesss@users.noreply.github.com>
2025-12-09 08:22:25 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ app.set('views','./views')
 
 // Define your routes
 require("../route/home")(app)
+require("../db/dbconnect")(app)
 require("../route/delnoticias")(app)
 require("../route/getnoticias")(app)
 require("../route/postnoticias")(app)
