21ca8bb5dc2a8c2491fd56851e8c80a17ea87a14
Lucasgomesss <168254568+Lucasgomesss@users.noreply.github.com>
2025-12-09 08:21:51 -0400

    Update getnoticias.js

--- route/getnoticias.js
+++ route/getnoticias.js
@@ -1,7 +1,9 @@
 module.exports = (app) => {
     app.get('/noticias', async (req, res) => {
+        await app.DBClient.connect(); //realizar a conexão com o banco 
       res.send("ola");
     })
 
 }
+
 
