5a65599c74e4ecb93b644ae5a9cd0456161feb57
Lucasgomesss <168254568+Lucasgomesss@users.noreply.github.com>
2025-12-09 08:23:05 -0400

    Update getnoticias.js

--- route/getnoticias.js
+++ route/getnoticias.js
@@ -1,9 +1,18 @@
 module.exports = (app) => {
     app.get('/noticias', async (req, res) => {
-        await app.DBClient.connect(); //realizar a conexão com o banco 
+        try {
+            await app.DBClient.connect(); //realizar a conexão com o banco 
+            const noticias = await app.DBClient.db('portalnoticias')
+                .collection('noticias').find().toArray();
+            res.json(noticias)
+        } finally {
+            // Ensures that the client will close when you finish/error
+            await app.DBClient.close();
+        }   await app.DBClient.connect(); //realizar a conexão com o banco 
       res.send("ola");
     })
 
 }
+
 
 
